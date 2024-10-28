0588d912b0d3cc46ff9f09b7dbfe1c72a9a22af1
Payal Gaikwad <gaikwadpayalr@gmail.com>
2024-10-28 23:23:23 +0530

    few changes in user routes

--- routes/user.js
+++ routes/user.js
@@ -26,25 +26,33 @@ router.get("/:id", async (req, res) => {
 
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
+  console.log(req.body);
   //Check if user already exists
   const userExists = await User.findOne({ email });
   if (userExists) {
     return res.status(400).json({
-      message: "User already exists",
+      message: "User already exists. Please login.",
     });
   }
-  //hashing password
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new User({
-    name,
-    email,
-    password: hashedPassword,
-  });
-  await newUser.save();
-  return res.status(201).json({
-    message: "User Created succesfully",
-    email,
-  });
+
+  try {
+    //hashing password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    return res.status(201).json({
+      message: "User Created succesfully",
+      email,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Unexpected error occurred. Please try again",
+    });
+  }
 });
 
 //User login
